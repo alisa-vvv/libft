2d8b3cb93648ccfab1401bc991cdd451a9b17587
Alisa Valiullina <avaliull@f1r4s7.codam.nl>
2024-10-07 14:56:47 +0200

    fix values tmrw

--- tester.c
+++ tester.c
@@ -6,24 +6,25 @@
 /*   By: avaliull <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2024/10/07 11:47:26 by avaliull       #+#    #+#                */
-/*   Updated: 2024/10/07 14:26:10 by avaliull       ########   odam.nl        */
+/*   Updated: 2024/10/07 14:51:04 by avaliull       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ctype.h>
 #include <stdio.h>
 
-int	ft_isalnum (char c)
+int	ft_isascii(char c)
 {
-	if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') ||
-		(c >= 'A' && c <= 'Z'))
-		return (8);
-	return (0);
+	if (c < 0 || c > 127)
+		return (0);
+	return (1);
 }
 
-int	main (int argc, char *argv[0])
-{
-	printf("result: %d\n", ft_isalnum(argv[1][0]));
-	printf("compare: %d\n", isalnum(argv[1][0]));
-	return (argc);
+int	main(void)
+{	
+	char test = '€';
+	char compare = '€'; 
+	printf("result: %d\n", ft_isascii(test));
+	printf("compare: %d\n", isascii(compare));
+	return (0);
 }
