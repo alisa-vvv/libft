2f4f1c94f7403c213f5a8cbb52271c57236924e1
Alisa Valiullina <avaliull@f1r4s7.codam.nl>
2024-10-07 14:31:56 +0200

    some funcs

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                         ::::::::           */
-/*   tester.c                                            :+:    :+:           */
+/*   ft_isalnum.c                                        :+:    :+:           */
 /*                                                      +:+                   */
 /*   By: avaliull <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2024/10/07 11:47:26 by avaliull       #+#    #+#                */
-/*   Updated: 2024/10/07 14:26:10 by avaliull       ########   odam.nl        */
+/*   Updated: 2024/10/07 14:30:19 by avaliull       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctype.h>
-#include <stdio.h>
-
-int	ft_isalnum (char c)
+int	ft_isalnum(char c)
 {
-	if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') ||
-		(c >= 'A' && c <= 'Z'))
+	if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z'))
 		return (8);
 	return (0);
-}
-
-int	main (int argc, char *argv[0])
-{
-	printf("result: %d\n", ft_isalnum(argv[1][0]));
-	printf("compare: %d\n", isalnum(argv[1][0]));
-	return (argc);
 }
--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                         ::::::::           */
-/*   tester.c                                            :+:    :+:           */
+/*   ft_isdigit.c                                        :+:    :+:           */
 /*                                                      +:+                   */
 /*   By: avaliull <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2024/10/07 11:47:26 by avaliull       #+#    #+#                */
-/*   Updated: 2024/10/07 14:26:10 by avaliull       ########   odam.nl        */
+/*   Updated: 2024/10/07 14:15:41 by avaliull       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctype.h>
-#include <stdio.h>
-
-int	ft_isalnum (char c)
+int	ft_isdigit(char c)
 {
-	if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') ||
-		(c >= 'A' && c <= 'Z'))
-		return (8);
+	if (c >= '0' && c <= '9')
+		return (2048);
 	return (0);
-}
-
-int	main (int argc, char *argv[0])
-{
-	printf("result: %d\n", ft_isalnum(argv[1][0]));
-	printf("compare: %d\n", isalnum(argv[1][0]));
-	return (argc);
 }
