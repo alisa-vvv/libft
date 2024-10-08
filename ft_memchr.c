39303205222d44f7de9f143263be7693cd0b535c
Alisa Valiullina <avaliull@f0r4s24.codam.nl>
2024-10-08 19:20:22 +0200

    some more

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,18 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tester.c                                           :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: avaliull <avaliull@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/07 11:47:26 by avaliull          #+#    #+#             */
-/*   Updated: 2024/10/08 19:16:36 by avaliull         ###   ########.fr       */
+/*   Created: 2024/10/08 19:17:50 by avaliull          #+#    #+#             */
+/*   Updated: 2024/10/08 19:17:54 by avaliull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
@@ -28,31 +24,3 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
-
-int	main(void)
-{	
-	unsigned char	*str;
-	str = memchr("ab0cde", '0', 4);
-	printf("Example_Res: %s\n", str);
-	str = ft_memchr("ab0cde", '0', 4);
-	printf("ft_Res: %s\n", str);
-	return (0);
-}
-
-
-
-
-	// while (i < 5)
-	// {
-	// 	printf("%d, ", ptr[i]);
-	// 	i++;
-	// }
-
-	// ft_memset(ptr, 9, 3);
-	// i = 0;
-	// printf("new arr: ");
-	// while (i < 5)
-	// {
-	// 	printf("%d, ", ptr[i]);
-	// 	i++;
-	// }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,21 @@
 /*   By: avaliull <avaliull@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 18:27:58 by avaliull          #+#    #+#             */
-/*   Updated: 2024/10/08 18:27:59 by avaliull         ###   ########.fr       */
+/*   Updated: 2024/10/08 18:47:43 by avaliull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	while ((*s1 || *s2) && n > 0)
+	{
+		if (*s1 < *s2)
+			return (-1);
+		if (*s1 > *s2)
+			return (1);
+		n--;
+		s1++;
+		s2++;
+	}
+	return (0);
+}
