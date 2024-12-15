b0e4fceca65c710c15d5bed7e8cd94f5ba432e07
Alisa Valiullina <avaliull@f0r2s5.codam.nl>
2024-12-15 15:20:37 +0100

    removed leftover comments

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: avaliull <avaliull@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 16:50:23 by avaliull          #+#    #+#             */
-/*   Updated: 2024/12/15 14:04:41 by avaliull       ########   odam.nl        */
+/*   Updated: 2024/12/15 15:19:48 by avaliull       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	else if (dest > src)
 	{
-	//	uc_src += n - 1;
-	//	uc_dest += n - 1;
-		while (n--)	
+		while (n--)
 			uc_dest[n] = uc_src[n];
-			//*uc_dest-- = *uc_src--;
 	}
 	return (dest);
 }
