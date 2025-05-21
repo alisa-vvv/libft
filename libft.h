544bb0cf0bdce0fc8003f06662934c652e9d3ba8
Alisa Valiullina <avaliull@f0r2s2.codam.nl>
2025-05-21 18:49:44 +0200

    added new functions to header

--- libft.h
+++ libft.h
@@ -23,11 +23,15 @@ typedef struct s_list
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
+void	free_2d_arr(void **arr);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
+int		ft_islower(int c);
 int		ft_isprint(int c);
+int		ft_isspace(int c);
+int		ft_isupper(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
